5b10010b2fed72d9bfde964013dc7f036dcae907
Obadia23 <osadock23@gmail.com>
2025-09-08 11:57:56 +0300

    week 3 submission

--- answers.sql
+++ answers.sql
@@ -1,1 +1,3 @@
-
+USE salesdb; CREATE TABLE student (id INT PRIMARY KEY, fullName VARCHAR(100) NOT NULL, age INT);
+INSERT INTO student (id, fullName, age) VALUES(1, 'Alice Johnson', 18), (2, 'Brian Smith', 19),(3, 'Clara Davis', 21);
+UPDATE student SET age = 20 WHERE id = 2;
